6c90802034ec4601bf900261d1a5a1fdfe873dde
Bunyod <bunyodbunyod035@gmail.com>
2022-04-21 12:27:05 +0500

    add project

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <TermRu />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import TermRu from "src/components/TermRu.vue";
+export default defineComponent({
+  name: "PageIndex",
+  components: {
+    TermRu,
+  },
+});
+</script>
